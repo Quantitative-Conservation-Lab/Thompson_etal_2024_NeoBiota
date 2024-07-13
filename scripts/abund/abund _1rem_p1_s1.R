d92bc4b96eb0097c34951c9835cbe1a40a37491d
briellekwarta19 <brielle.kwarta@gmail.com>
2024-07-13 17:25:54 -0500

    confirm

--- scripts/abund/abund _1rem_p1_s1.R
+++ scripts/abund/abund _1rem_p1_s1.R
@@ -144,6 +144,10 @@ for(p in 1:P){
   L.annual[4,4,p] <- (survival_age[4,p]^12)
 }
 
+#### checking ####
+library(expm)
+all.equal(L.annual[,,4], L.june[,,4] %*% (L.notjune[,,4] %^% 11))
+
 stable.dist <- array(NA, dim = c(4,P))
 
 for(p in 1:P){
